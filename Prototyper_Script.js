b7ce7e83a89a306aaace02116c43b2ad96684ce5
John He <aoensblend@gmail.com>
2016-02-16 16:50:32 +1300

    Added the clear canvas code
    clearCanvas();

--- Prototyper_Script.js
+++ Prototyper_Script.js
@@ -200,7 +200,17 @@ function drawText(x,y) {
 
     context.font = "20px Arial";
     context.fillText(input, x, y);
+}
+	
+	/**
+ * Clears the canvas board.
+ */
+function clearCanvas() {
+    var canvas = document.getElementById("myCanvas");
+    var context = canvas.getContext("2d");
 
-    }
+    context.clearRect(0, 0, canvas.width, canvas.height);
+}
+
 
 
