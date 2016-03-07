b62c8f86f6951bc118491e4985f60e78c99a7a31
ieuanmorris <sp33dracist@gmail.com>
2016-03-07 13:03:29 +1300

    Update register.php

--- register.php
+++ register.php
@@ -6,59 +6,74 @@ ieuan morris ID 1390482
 
 	require_once("db_settings.php");
 	
-	$username = $_POST['username'];
-	$email = $_POST['email'];
-	$phoneNumber = $_POST['phoneNumber'];
-	$altContact = $_POST['altContact'];
-	$password = $_POST['password'];
-	$confirmPassward = $_POST['confirmPassward'];
-
 	//creating the connection.
-	$conn = new mysqli ($hostname, $username, $password, $database);
+	$con = mysqli_connect ($hostname,$username,$password,$database);
 
-	//check connection
-	if ($conn->connect_error) {
-		die("connection failed:" . $conn->connect_error);
-	}
+	if ($con)
+{
+	//checks to see if the DB exists, if it doesnt one is created.
+	if (mysqli_num_rows( mysqli_query($con,"SHOW TABLES LIKE 'loginandregister';") ) == 0)
+	{
+		mysqli_query($con,
+			"CREATE TABLE IF NOT EXISTS `loginandregister` (
+			  `id` int(11) NOT NULL, 
+			  `userName` varchar(50) NOT NULL,
+			  `email` varchar(50) NOT NULL,
+			  `phoneNumber` int(50)  NULL,
+			  `altContact` int(50)  NULL,
+			  `password` varchar(50) NOT NULL,
+			  `confirmPassward` varchar (50) NOT NULL,
+			  
+			  PRIMARY KEY (`id`)
+			)"
+		);
+	} 
 
-	if(empty($username)) {
-		echo "User Name cannot be left blank";
-		die();
-	}
-
-	if(empty($email)) {
-		echo"please fill in your email address";
-	}
-
-	if(empty($phoneNumber)) {
-		echo"please enter a phone number";
-	}
+	if(isset($_POST['id']) 
+	&& isset($_POST['userName']) 
+	&& isset($_POST['email']) 
+	&& isset($_POST['ph']) 
+	&& isset($_POST['alt']) 
+	&& isset($_POST['pass']) 
+	&& isset($_POST['confirm']) 
 	
-	if(empty($altContact)){
-		echo"please enter an alternate contact.";
-		die();
-	}
+	{
+		$id = $_POST['id'];
+		$userName = $_POST['userName'];
+		$email = $_POST['email'];
+		$phoneNumber = $_POST['ph'];
+		$altContact = $_POST['alt'];
+		$password = $_POST['pass'];
+		$confirmPassward = $_POST['confirm'];
 
-	if(empty($password)) {
-		echo"please enter a password";
-	}
-
-	if(empty($confirmPassward)) {
-		echo"please re enter your password";
-	}
-
+	
 
 	//inserting query.
 
-	$sql = "INSERT INTO loginandregister (userName, email, phoneNumber, altContact, password, confirmPassward)
-	VALUES ('$username', '$email', '$phoneNumber', '$altContact', '$password', '$confirmPassward')";
-
-	if($conn->query($sql)== TRUE) {
-		echo "Thank You for registering with us";
-	}else{
-		echo"ERROR " . $sql . "<br>" . $conn->error;
+	$query = "INSERT INTO loginandregister 
+	(userName, email, phoneNumber, altContact, password, confirmPassward)
+	VALUES 
+	('$username', '$email', '$phoneNumber', '$altContact', '$password', '$confirmPassward')";
+	
+if (mysqli_query($con,$query)) 
+		{
+			//adding to database was successfull
+			echo "Thank you for registering your account."
+		}
+		else
+		{
+			//adding to the database came accross an error
+			echo "something went wrong with your registration, please try again";
+		}
+											  
 	}
+	//POST variables not assigned properly
+	else {	echo "error fetching data";	}
+}
+//database connection error
+else { echo "Failed to connect to MySQL: " . mysqli_connect_error(); 
+}
 
-	$conn->close();
+
 
 ?>
