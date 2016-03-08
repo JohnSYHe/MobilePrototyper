45dc6deb01045464a0685ef46407f62932463914
John He <aoensblend@gmail.com>
2016-03-08 15:22:32 +1300

    Antonies Code
    Antonies working code for the re-drawing. Added comments and console logs for debugging.

--- Prototyper_Script.js
+++ Prototyper_Script.js
@@ -1,10 +1,9 @@
 // CANVAS SIZING AREA - BELOW!!
+
 /**
  * Sets the canvas size to: small.
  */
 function sCanvas() {
-    // Gets the canvas id from the html file and stores it in the variable 'canvas' for editing.
-    var canvas = document.getElementById("myCanvas");
     // Create the canvas with these attributes.
     canvas.width = 320;
     canvas.height = 426;
@@ -14,8 +13,6 @@ function sCanvas() {
  * Sets the canvas size to: medium.
  */
 function mCanvas() {
-    // Gets the canvas id from the html file and stores it in the variable 'canvas' for editing.
-    var canvas = document.getElementById("myCanvas");
     // Create the canvas with these attributes.
     canvas.width = 320;
     canvas.height = 470;
@@ -25,63 +22,53 @@ function mCanvas() {
  * Sets the canvas size to: large.
  */
 function lCanvas() {
-    // Gets the canvas id from the html file and stores it in the variable 'canvas' for editing.
-    var canvas = document.getElementById("myCanvas");
     // Create the canvas with these attributes.
     canvas.width = 480;
     canvas.height = 640;
 }
+
 // CANVAS SIZING AREA - ABOVE!!
 
 var canvas, context;
-
+	
 /**
  * Initialise everything for canvas and context.
  */
 function init() {
-
     // Find the canvas element.
     canvas = document.getElementById('myCanvas');
     if (!canvas) {
         alert('Error: I cannot find the canvas element!');
         return;
     }
-
     if (!canvas.getContext) {
         alert('Error: no canvas.getContext!');
         return;
     }
-
     // Get the 2D canvas context.
-
     context = canvas.getContext('2d');
     if (!context) {
         alert('Error: failed to getContext!');
         return;
     }
-
     // Starts the mouse controller to enable the event listeners for user interaction.
-	mouseController(canvas, context);
+    mouseController(canvas, context);
 }
 
-// Global Mouse Positions.
+// Global Mouse Positions
 var xPosition = 0;
 var yPosition = 0;
-
-//Global variable for which widget is selected.
-var widgetSelected = null;
-
-// Array for all the objects.
-var objectArray = [];
-// Counts the amount of objects created.
-var objectCounter = 0;
+// Global variable for which widget is selected.
+var widgetSelection = null;
+// Array to store the widgets.
+var widgetArray = [];
+var counter = 1;
 
 /**
  * Controller for the mouse events.
  * @param canvas
  */
 function mouseController(canvas, context) {
-	
 	// Event Listeners for the mouse.
 	canvas.addEventListener("mousemove", positionManager);
 	canvas.addEventListener("mouseout", hideCoordinates);
@@ -102,176 +89,194 @@ function mouseController(canvas, context) {
     }	
 	
 	/**
-	 * @param event Event listener for the mouse and for it to work with Firefox.
+	 * 
+	 * @param event 
 	 */
 	function positionManager(event) {
 		var mousePos = getMousePosition(canvas, event);
 		// Formats a message that shows the coordinates.
-		var mouseCoordinates = "Coordinates: " + mousePos.x + "-X, " + mousePos.y + "-Y ";
+		var mouseCoordinates = 'Coordinates: ' + mousePos.x + ',' + mousePos.y;
 		
 		xPosition = mousePos.x;
 		yPosition = mousePos.y;
 		
 		// Sends the message to be displayed.
 		displayCoordinates(mouseCoordinates);
+    }
 		
+    /**
+     * Sets and displays the co-ordinates below the canvas.
+     * @param coordinates = the co-ords to be displayed
+     */
+    function displayCoordinates(coordinates){
+        document.getElementById("mCoordinates").innerHTML = coordinates;
     }
 	
-	/**
-	 * Sets and displays the co-ordinates below the canvas.
-	 * @param coordinates = the co-ords to be displayed
-	 */
-	function displayCoordinates(coordinates){
-		document.getElementById("mCoordinates").innerHTML = coordinates;
-	}
-
-	/**
-	 * Hides the mouse position when it leaves the canvas.
-	 */
-	function hideCoordinates() {
-		document.getElementById("mCoordinates").innerHTML = "";
-	}
+    /**
+     * Hides the mouse position when it leaves the canvas.
+     */
+    function hideCoordinates() {
+        document.getElementById("mCoordinates").innerHTML = "";
+    }
 }
 
 /**
- * Gets the value of the widget selected on the sidebar.
+ * Receives user onClick and selects the widget to be drawn.
  */
 function setDrawingMode(btnValue) {
-    widgetSelected = btnValue;
+    widgetSelection = btnValue;
 }
 
 /**
- * Draw method. Currently set to only call the draw square method.
+ * Widget Controller Method.
  */
 function widgetController() {
+
+	var createdWidget;
 	
-    if (widgetSelected == null) {
-		widgetScanner();
-	} else if (widgetSelected == 0) {
-        drawSquare(xPosition, yPosition);
-    } else if (widgetSelected == 1) {
-        drawCircle(xPosition, yPosition)
-    } else if (widgetSelected == 2) {
-        drawText(xPosition, yPosition)
-    }
-	
-	/**
-	 * Console notifications for debugging.
-	 */
-	function objectArrayNotification() {
-		// Displays the details of the object being put into the array.
-		console.log(objectArray[objectCounter]);
-		// increments the counter up so it displays the next object values.
-		objectCounter++;
-		// How many elements are in the array.
-		console.log("Total Objects in Array: " + objectArray.length);
-		
-		for (i = 0; i < objectArray.length; i++) {
-			console.log("What is inside the array: " + objectArray[i].arrayObject);
-		}
-	}
-	
-	/**
-	 * If there are no widgets selected, this will scan where the mouse clicks
-	 * on the canvas and if it clicked on a widget. In progress.
-	 */
-	function widgetScanner() {
-	
-		if (objectArray.length == 0) {
-			console.log("There doesn't seem to be anything here....");
+	if (widgetSelection == null) {
+		if (widgetArray.length == 0) {
+				console.log("There doesn't seem to be anything here....");
 		} else {
-			for (i = 0; i < objectArray.length; i++) {
-				if (objectArray[i].arrayObject.includes("square")) {
-					console.log(objectArray[i].arrayObject);
+			for (i = 0; i < widgetArray.length; i++) {
+				//Test code to check whats in the widgetArray array.
+				for(i = 0; i < widgetArray.length; i++){
+					// Displays the all the widgets in the array.
+					console.log(widgetArray[i]);
 				}
 			}
 		}
-	}
-	
-	/**
-	 * Draws a square onto the canvas when it is called.
-	 * This can be further improved to be a textbox or a button.
-	 */
-	function drawSquare() {
-		var squareWidth = 100;
-		var squareLength = 100;
-		
-		// Make the rectangle blue.
-		context.fillStyle = "blue";
-		// Place a rectangle centered at mouse position.
-		context.fillRect((xPosition - squareWidth / 2), 
-			(yPosition - squareLength / 2), squareWidth, squareLength);
-		
-		// Adds the square object into the array.
-		objectArray.push({arrayObject: "square" + objectCounter, xPos: xPosition, 
-			yPos: yPosition, width: squareWidth, length: squareLength});
-		
-		// Notify console for testing.
-		objectArrayNotification();
-		// Disables the selected widget to prevent spam.
-		widgetSelected = null;		
-	}
-	
-	/**
-	 * Draws a circle onto the canvas when it is called.
-	 * This can be useful for something.
-	 */
-	function drawCircle() {
-		var circleRadius = 50;
-		var circleStartAngle = 0;
-		var circleEndAngle = 2 * Math.PI;
-	
-		// Draws the circle.
+	} else if (widgetSelection == 0) {
+		createSquare.prototype.draw = function(context) {
+		context.fillRect(this.x, this.y, this.width, this.height);
+		// Parameters explanation for the rectangle: (X-Position, Y-Position, width, height			
+		}
+		 createdWidget = new createSquare();	
+		//createWidget();
+	} else if (widgetSelection == 1) {
+		createCircle.prototype.draw = function(context) {
 		context.beginPath();
-		context.arc(xPosition, yPosition, circleRadius, circleStartAngle, circleEndAngle);
-		context.stroke();
+		context.arc(this.x, this.y, this.rad, 0, 2 * Math.PI);
+		context.stroke();			
+		// Parameters explanation for the circle: (x-position, y-position, radius, starting angle, ending angle)
+		}
+		 createdWidget = new createCircle();		
+	} else if (widgetSelection == 2) {
+		drawText(xPosition, yPosition);
+	}
+	
+	// function createWidget() {
+		// createdWidget.draw(context);			
+		// createdWidget.name = "Button " + counter;
+		// counter++;
+		// widgetArray.push(createdWidget);
+		// console.log(widgetArray[counter]);
+	// }
+	
+	// function insertedWidget() {
+		// console.log("Drew: " + widgetArray);
+	// }
+	
+	// If createdWidget is not null, create the widget and add it to the array.
+	if (createdWidget != null) {	
+		for (i = 0; i <= widgetArray.length; i++){
+			createdWidget.draw(context);			
+			createdWidget.name = "Button " + counter;
+			counter++;
+			widgetArray.push(createdWidget);
 		
-		// Adds the circle object into the array.
-		objectArray.push({arrayObject: "circle" + objectCounter, xPos: xPosition,
-			yPos: yPosition, sAngle: circleStartAngle, eAngle: circleEndAngle});
-		
-		// Notify console for testing.
-		objectArrayNotification();
-		// Disables the selected widget to prevent spam.
-		widgetSelected = null;
+			//Test code to check what's in the widgetArray array.
+			for(i = 0; i < widgetArray.length; i++){
+				// Displays the all the widgets in the array.
+				console.log(widgetArray[i]);
+			}
+			// Developer - Displays the amount of objects in the array.
+			console.log("Total Objects in Array:" + widgetArray.length);	
+			widgetSelection = null;
+		}
+	}
+	
+	/**
+	 * Creates a square object with multiple parameters.
+	 */
+	function createSquare() {
+		this.name = name || 'undefined';
+		this.x = xPosition || 0;
+		this.y = yPosition || 0;
+		this.width = 100 || 200;
+		this.height = 100 || 200;
+	}
+
+	/**
+	 * Creates a circle object with multiple parameters.
+	 */
+	function createCircle() {
+		this.name = name || 'undefined';
+		this.x = xPosition || 0;
+		this.y = yPosition || 0;
+		this.rad = 50 || 100; //Default size 50? needs reconsidering when changing size is implemented
 	}
 
 	/**
 	 * Draws the text that the user inputs
 	 * @param x X-Position of the mouse to be placed on the canvas.
 	 * @param y Y-Positition of the mouse to be placed on the canvas.
-	 */
+	*/
 	function drawText(x,y) {
-
 		var input = prompt("Enter text below", "Here...");
-
 		context.font = "20px Arial";
-		context.fillText(input, xPosition, yPosition);
-		
-		widgetSelected = null;
+		context.fillText(input, x, y);
+		widgetSelection = null;
 	}
 }
-	
+
 /**
  * Clears the canvas board.
  */
 function clearCanvas() {
-	// Clears the canvas.
+	// Remove everything drawn on the canvas.
     context.clearRect(0, 0, canvas.width, canvas.height);
-	// Calls the function to remove all the objects on the array.
-	clearArray();
+	// Informs developer how much objects were deleted.
+	console.log("Deleted: " + (counter - 1) + " widgets!");
+	// Call clearWidgetArray() to remove everything from the array when clearing canvas.
+	clearWidgetArray();
 }
 
 /**
- * Clears and rests the array.
+ * Clears the array containing all the widgets and resets it for further use.
+ * Useful for creating a new canvas.
  */
-function clearArray() {
-	// Displays how many objects are deleted.
-	console.log("Deleted " + objectArray.length + " objects!");
-	// Nullifies the array
-	objectArray = null;
-	// Redeclare the array.
-	objectArray = [];
-	// Reset the counter.
-	objectCounter = 0;
+function clearWidgetArray(){
+	// Set the array to null.
+	widgetArray = null;
+	// Make array undefined.
+	widgetArray = [];
+	// Reset counter back to 1.
+	counter = 1;
+}
+
+/**
+ * Draws all the widgets in the array.
+ */
+function drawWidgetArray(){
+	// Loop that draws the objects in the array one by one.
+	for (i = 0; i < widgetArray.length; i++) {
+		if (i == 0) {
+			console.log("Showing widgets... " + widgetArray.length + " found!");
+		}
+		widgetArray[i].draw(context);
+		console.log("->Re-drew widget: " + i + " " + widgetArray[i].name);
+	}
+	// Lets developer know that nothing is in the array.
+	if (widgetArray.length == 0) {
+		console.log("There's nothing to show!");
+	}
+}
+
+/**
+ * Hides all the widgets in the array.
+ */
+function hideWidgets() {
+	// Cleans out canvas.
+	context.clearRect(0, 0, canvas.width, canvas.height);
 }
