95d1624ade199465e8be6c1908eafc95ab40cdd4
aoensblend <aoensblend@gmail.com>
2016-06-12 19:00:51 +1200

    Prototyper Cleanup
    Moved the prototype stuff into a separate folder. Corrected links and
    references. Separated some functionality of the javascript into
    individual files for easier viewing and editing.

---
+++ applicationPage/Canvas_Manipulation.js
@@ -0,0 +1,63 @@
+/**
+* Clears the canvas board.
+*/
+function clearCanvas() {
+  // Remove everything drawn on the canvas.
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  // Clears out console.
+  console.clear();
+  // Informs developer how much objects were deleted.
+  console.log("Deleted: " + (widgetArray.length) + " widgets!");
+  // Call clearWidgetArray() to remove everything from the array when clearing canvas.
+  clearWidgetArray();
+  updateCommentSideBar();
+}
+
+// _________________________ CANVAS SIZING AREA - BELOW!!
+
+/**
+* Sets the canvas size to: small.
+*/
+function sCanvas() {
+  var warnUser = confirm("This will clear the canvas if you proceed!");
+
+  if (warnUser == true) {
+    // Create the canvas with these attributes.
+    canvas.width = 320;
+    canvas.height = 426;
+  } else {
+    // Do nothing.
+  }
+}
+
+/**
+* Sets the canvas size to: medium.
+*/
+function mCanvas() {
+  var warnUser = confirm("This will clear the canvas if you proceed!");
+
+  if (warnUser == true) {
+    // Create the canvas with these attributes.
+    canvas.width = 320;
+    canvas.height = 470;
+  } else {
+    // Do nothing.
+  }
+}
+
+/**
+* Sets the canvas size to: large.
+*/
+function lCanvas() {
+
+  var warnUser = confirm("This will clear the canvas if you proceed!");
+  if (warnUser == true) {
+    // Create the canvas with these attributes.
+    canvas.width = 480;
+    canvas.height = 640;
+  } else {
+    // Do nothing.
+  }
+}
+
+// _________________________ CANVAS SIZING AREA - ABOVE!!
